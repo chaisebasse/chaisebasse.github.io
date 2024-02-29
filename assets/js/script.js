a5e15b35b7839a27944d71722ac1e614e4a5761d
Ermanno di Giulio <digiulioermanno@gmail.com>
2024-02-29 15:52:48 +0100

    fixed a few things, added leaflet map to contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,9 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
+// Leaflet map
+var map = L.map('map').setView([48.858242, 2.29448], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
